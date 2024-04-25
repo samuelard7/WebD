d8010d91c62f25534fb920e95aef0c1bb96d587d
Richard Samuel <98638849+samuelard7@users.noreply.github.com>
2024-04-25 14:35:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 from flask import Flask, render_template, redirect, url_for,current_app, flash, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import UUID
 from forms import RegisterForm
+import psycopg2
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///user.db'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://quiztriviadb:password@localhost/user'
 app.config['SECRET_KEY']='6871ff06b5fb8f0fdd73300f'
 db = SQLAlchemy(app)
 
 app.app_context().push()
 
 
+def db_conn():
+    conn = psycopg2.connect(dbname = "user", 
+                        user = "richard_quant", 
+                        host= 'localhost',
+                        password = "$*CO8I3m",
+                        port = 5432)
 
-class User(db.Model):
+   
+    return conn
+
+         
+# class Entries(db.Model):
+#     __tablename__='owned_user'
+#     id = db.Column(db.Integer(),primary_key=True)
+#     category = db.Column(db.String(length=20),nullable=False)
+#     type = db.Column(db.String(length=20),nullable=False)
+#     difficulty = db.Column(db.String(length=10),nullable=False)
+#     result = db.Column(db.String(length=10))
+#     userrelate = db.Column(db.Integer(),db.ForeignKey('user.id'))
+class userinfo(db.Model):
+   
     id = db.Column(db.Integer(),primary_key=True)
     username = db.Column(db.String(length=30),nullable=False,unique=True)
     password = db.Column(db.String(length=20),nullable=False)
     email_address = db.Column(db.String(length=50),nullable=False)
-    entry = db.relationship('Entries',backref='owned_user',lazy=True)
-        
-class Entries(db.Model):
-    id = db.Column(db.Integer(),primary_key=True)
-    category = db.Column(db.String(length=20),nullable=False)
-    type = db.Column(db.String(length=20),nullable=False)
-    difficulty = db.Column(db.String(length=10),nullable=False)
-    result = db.Column(db.String(length=10))
-    userrelate = db.Column(db.Integer(),db.ForeignKey('user.id'))
-
-
+    # entry = db.relationship('Entries',backref='owned_user',lazy=True)
+  
 @app.route('/')
 @app.route('/Front')
 def front_page():
@@ -41,7 +53,7 @@ def home_page():
 def about_us():
     return render_template('About.html')
 
-@app.route('/Login')
+@app.route('/LoginPage')
 def login_page():
     return render_template('Login.html')
 
@@ -49,20 +61,42 @@ def login_page():
 def contact_page():
     return render_template('Contact.html')
 
+@app.route('/LoginPage',methods=['GET','POST'])
+def search_data():
+    usernamefield = request.form['user_name']
+    password_field = request.form['pass_word']
+    conn = db_conn()
+    cur=conn.cursor()
+    cur.execute('''select * from userinfo''')
+    data = cur.fetchall()
+    for i in range(len(data)):
+        user_name = data[i][1]
+        pass_word = data[i][2]
+        if user_name == usernamefield and password_field == pass_word:
+            return render_template('Success_HomePage.html',name=usernamefield)
+        flash(f'User Not Registered',category="danger")
+    return render_template('Login.html')
 @app.route('/Register',methods=['GET','POST'])
 def register_page():
     form = RegisterForm()
+    conn = db_conn()
+    cur = conn.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS userinfo 
+            (id SERIAL PRIMARY KEY,
+            username varchar(50),
+            password varchar(50),
+            email_address varchar(50));
+            ''')
     if form.validate_on_submit():
-        user_to_create=User(username=form.username.data,
-                            password=form.passw.data,
-                            email_address=form.email_address.data,
-                            ) # type: ignore
-        db.session.add(user_to_create)
-        db.session.commit()
+        cur.execute(
+            '''INSERT INTO userinfo(username,password,email_address) VALUES(%s,%s,%s)''',(form.username.data,form.passw.data,form.email_address.data))
         if request.method == 'POST':
             name = request.form.get('username')
+            conn.commit()
+            cur.close()
+            conn.close()
             return render_template('Success_HomePage.html',name=name)
-        return redirect(url_for('home_page'))
+        return redirect(url_for('home_page',message="Failed"))
     if form.errors != {}:
         for err_msg in form.errors.values():
             flash(f'Error: {err_msg}', category="danger")
@@ -79,3 +113,6 @@ def __repr__(self):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
