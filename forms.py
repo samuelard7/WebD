6d651503c5c42c284eaa99cad8b03e5d34cb58a0
Richard Samuel <98638849+samuelard7@users.noreply.github.com>
2024-09-14 14:25:23 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -6,17 +6,17 @@ class RegisterForm(FlaskForm):
     username = StringField(label='Username', validators=[Length(min=5, max=15) , DataRequired()]) # type: ignore
     passw = PasswordField(label='Password', validators= [Length(min=6), DataRequired()]) 
     cpassw = PasswordField(label='Confirm Password', validators=[EqualTo('passw'), DataRequired()])
-    email_address =StringField(label='Email', validators=[Email()])
+    email_address =StringField(label='Email', validators=[DataRequired()])
     submit = SubmitField(label='Submit')
     
 class Recovery(FlaskForm):
-    email_add =StringField(label='Email', validators=[Email()])
+    email_add =StringField(label='Email', validators=[DataRequired()])
     otp = PasswordField(label='OTP',validators=[Length(max=6)])
     submit = SubmitField(label='Submit')
     
 class QueryForm(FlaskForm):
     person_name = StringField(label='Username', validators=[Length(min=5, max=15) , DataRequired()])
-    person_email =StringField(label='Email', validators=[Email(), DataRequired()])
+    person_email =StringField(label='Email', validators=[DataRequired()])
     person_mobile=StringField(label='Mobile',validators=[Length(min=10, max=10), DataRequired()])
     person_message=StringField(label='Message', validators=[Length(max=300), DataRequired()])
     submit = SubmitField(label='Submit')
